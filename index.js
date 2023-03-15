6ed67425c37ac09fe23d8cae57285a9b87871765
Patricia <wambuipatricia3@gmail.com>
2023-03-15 19:54:11 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+// Code your solution in this file
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
+  
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  }
+  
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
+
+
+
+
+
+
+
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    }
+  }
+  
+  function fareDoubler(fare) {
+    const doubleFare = createFareMultiplier(2);
+    return doubleFare(fare);
+  }
+  
+  function fareTripler(fare) {
+    const tripleFare = createFareMultiplier(3);
+    return tripleFare(fare);
+  }
+  
+  function selectDifferentDrivers(drivers, selectDriversFunction) {
+    return selectDriversFunction(drivers);
+  }
+
